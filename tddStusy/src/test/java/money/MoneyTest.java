7fcf7d9c2ac97fe2d45bc5b7984f8b4dbd9f7898
dennisvirus <dennisvirus@gmail.com>
2016-04-19 17:51:50 +0900

    깃 등록

---
+++ tddStusy/src/test/java/money/MoneyTest.java
@@ -0,0 +1,16 @@
+package money;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by yb.choi on 16. 4. 19..
+ */
+public class MoneyTest {
+
+    @Test
+    public void testTest(){
+        assertEquals(10,10);
+    }
+}
