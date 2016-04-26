7816b245c690a70e17214a8b1c8fad5daafb98ca
dennisvirus <dennisvirus@gmail.com>
2016-04-26 13:11:29 +0900

    4장

--- tddStusy/src/test/java/money/MoneyTest.java
+++ tddStusy/src/test/java/money/MoneyTest.java
@@ -3,6 +3,8 @@ package money;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by yb.choi on 16. 4. 19..
@@ -12,21 +14,30 @@ public class MoneyTest {
     @Test
     public void testMultiplication(){
         Dollar five = new Dollar(5);
-        Dollar product = five.times(2);
-        assertEquals(10, product.amount);
-        product=five.times(3);
-        assertEquals(15, product.amount);
+        assertEquals(new Dollar(10), five.times(2));
+        assertEquals(new Dollar(15), five.times(3));
+    }
+
+    @Test
+    public void testEquality(){
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
     }
 
     class Dollar{
 
-        int amount;
+        private int amount;
         Dollar(int amount){
             this.amount = amount;
         }
 
         Dollar times(int multiplier){
             return new Dollar(amount * multiplier);
+        }
+
+        public boolean equals(Object object){
+            Dollar dollar = (Dollar) object;
+            return amount == dollar.amount;
         }
     }
 }
